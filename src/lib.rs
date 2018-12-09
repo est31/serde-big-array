6043635dcf2f768ce387f926db3b0f2123a734d4
est31 <MTest31@outlook.com>
2018-12-09 17:36:01 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+
+extern crate core;
+
+#[doc(hidden)]
+pub mod reex {
+    pub use core::fmt;
+    pub use core::marker::PhantomData;
+    pub use serde::ser;
+    pub use serde::ser::{Serialize, Serializer};
+    pub use serde::de::{Deserialize, Deserializer, Visitor, SeqAccess, Error};
+}
+
+#[macro_export]
+macro_rules! big_array {
+    ($name:ident; $($len:expr,)+) => {
+        pub trait $name<'de>: Sized {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                where S: $crate::reex::Serializer;
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+                where D: $crate::reex::Deserializer<'de>;
+        }
+        $(
+            impl<'de, T> $name<'de> for [T; $len]
+                where T: Default + Copy + $crate::reex::Serialize + $crate::reex::Deserialize<'de>
+            {
+                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                    where S: $crate::reex::Serializer
+                {
+                    use $crate::reex::ser::SerializeTuple;
+                    let mut seq = serializer.serialize_tuple(self.len())?;
+                    for elem in &self[..] {
+                        seq.serialize_element(elem)?;
+                    }
+                    seq.end()
+                }
+
+                fn deserialize<D>(deserializer: D) -> Result<[T; $len], D::Error>
+                    where D: $crate::reex::Deserializer<'de>
+                {
+                    use $crate::reex::PhantomData;
+                    struct ArrayVisitor<T> {
+                        element: PhantomData<T>,
+                    }
+
+                    impl<'de, T> $crate::reex::Visitor<'de> for ArrayVisitor<T>
+                        where T: Default + Copy + $crate::reex::Deserialize<'de>
+                    {
+                        type Value = [T; $len];
+
+                        fn expecting(&self, formatter: &mut $crate::reex::fmt::Formatter) -> $crate::reex::fmt::Result {
+                            formatter.write_str(concat!("an array of length ", $len))
+                        }
+
+                        fn visit_seq<A>(self, mut seq: A) -> Result<[T; $len], A::Error>
+                            where A: $crate::reex::SeqAccess<'de>
+                        {
+                            let mut arr = [T::default(); $len];
+                            for i in 0..$len {
+                                arr[i] = seq.next_element()?
+                                    .ok_or_else(|| $crate::reex::Error::invalid_length(i, &self))?;
+                            }
+                            Ok(arr)
+                        }
+                    }
+
+                    let visitor = ArrayVisitor { element: PhantomData };
+                    deserializer.deserialize_tuple($len, visitor)
+                }
+            }
+        )+
+    }
+}
---
+++ tests/basic.rs
@@ -0,0 +1,28 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_big_array;
+
+//use serde::de::{Deserialize, Deserializer, Visitor, SeqAccess, Error};
+
+big_array! {
+    BigArray;
+    40, 48, 50, 56, 64, 72, 96, 100, 128, 160, 192, 200, 224, 256, 384, 512,
+    768, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
+}
+
+#[derive(Serialize, Deserialize)]
+struct S {
+    #[serde(with = "BigArray")]
+    arr: [u8; 64],
+}
+
+#[test]
+fn test() {
+    let s = S { arr: [1; 64] };
+    let j = serde_json::to_string(&s).unwrap();
+    let s_back = serde_json::from_str::<S>(&j).unwrap();
+    assert!(&s.arr[..] == &s_back.arr[..]);
+}
