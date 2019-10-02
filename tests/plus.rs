64b2529b3002f0229acab99de5f5667aeab65836
est31 <MTest31@outlook.com>
2019-10-02 08:00:39 +0200

    Support mode that doesn't override defaults
    Fixes #4

---
+++ tests/plus.rs
@@ -0,0 +1,27 @@
+#![no_std]
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_big_array;
+
+big_array! { BigArray; +127, }
+
+#[derive(Serialize, Deserialize)]
+struct S {
+    #[serde(with = "BigArray")]
+    arr: [u8; 64],
+    #[serde(with = "BigArray")]
+    arr_2: [u8; 127],
+}
+
+#[test]
+fn test() {
+    let s = S { arr: [1; 64], arr_2: [1; 127] };
+    let j = serde_json::to_string(&s).unwrap();
+    let s_back = serde_json::from_str::<S>(&j).unwrap();
+    assert!(&s.arr[..] == &s_back.arr[..]);
+    assert!(&s.arr_2[..] == &s_back.arr_2[..]);
+}
