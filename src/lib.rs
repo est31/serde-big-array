b53576b4f35ba8bee7ac298c71cfa3d1d4c03057
est31 <MTest31@outlook.com>
2021-04-15 14:18:37 +0200

    Remove the const-generics feature and enable it by default
    Re-implement the big_array! macro in terms of the const-generics feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(feature = "const-generics"), forbid(unsafe_code))]
-
 /*!
 Big array helper for serde.
 The purpose of this crate is to make (de-)serializing arrays of sizes > 32 easy.
@@ -60,19 +58,7 @@ fn test() {
 */
 #![no_std]
 
-#[doc(hidden)]
-pub mod reex {
-    pub use core::fmt;
-    pub use core::result;
-    pub use core::marker::PhantomData;
-    pub use serde::ser;
-    pub use serde::ser::{Serialize, Serializer};
-    pub use serde::de::{Deserialize, Deserializer, Visitor, SeqAccess, Error};
-}
-
-#[cfg(feature = "const-generics")]
 mod const_generics;
-#[cfg(feature = "const-generics")]
 pub use const_generics::BigArray;
 
 /**
@@ -145,72 +131,7 @@ pre-defined set of array lengths. If omitted, it's implemented for the specified
 #[macro_export]
 macro_rules! big_array {
     ($name:ident; $($len:expr),+ $(,)?) => {
-        pub trait $name<'de>: Sized {
-            fn serialize<S>(&self, serializer: S) -> $crate::reex::result::Result<S::Ok, S::Error>
-                where S: $crate::reex::Serializer;
-            fn deserialize<D>(deserializer: D) -> $crate::reex::result::Result<Self, D::Error>
-                where D: $crate::reex::Deserializer<'de>;
-        }
-        $(
-            impl<'de, T> $name<'de> for [T; $len]
-                where T: Default + Copy + $crate::reex::Serialize + $crate::reex::Deserialize<'de>
-            {
-                fn serialize<S>(&self, serializer: S) -> $crate::reex::result::Result<S::Ok, S::Error>
-                    where S: $crate::reex::Serializer
-                {
-                    use $crate::reex::ser::SerializeTuple;
-                    let mut seq = serializer.serialize_tuple(self.len())?;
-                    for elem in &self[..] {
-                        seq.serialize_element(elem)?;
-                    }
-                    seq.end()
-                }
-
-                fn deserialize<D>(deserializer: D) -> $crate::reex::result::Result<[T; $len], D::Error>
-                    where D: $crate::reex::Deserializer<'de>
-                {
-                    use $crate::reex::PhantomData;
-                    struct ArrayVisitor<T> {
-                        element: PhantomData<T>,
-                    }
-
-                    impl<'de, T> $crate::reex::Visitor<'de> for ArrayVisitor<T>
-                        where T: Default + Copy + $crate::reex::Deserialize<'de>
-                    {
-                        type Value = [T; $len];
-
-                        fn expecting(&self, formatter: &mut $crate::reex::fmt::Formatter) -> $crate::reex::fmt::Result {
-                            macro_rules! write_len {
-                                ($l:literal) => {
-                                    write!(formatter, concat!("an array of length ", $l))
-                                };
-                                ($l:tt) => {
-                                    write!(formatter, "an array of length {}", $l)
-                                };
-                            }
-
-                            write_len!($len)
-                        }
-
-                        fn visit_seq<A>(self, mut seq: A) -> $crate::reex::result::Result<[T; $len], A::Error>
-                            where A: $crate::reex::SeqAccess<'de>
-                        {
-                            let mut arr = [T::default(); $len];
-                            for i in 0..$len {
-                                arr[i] = seq.next_element()?
-                                    .ok_or_else(|| $crate::reex::Error::invalid_length(i, &self))?;
-                            }
-                            Ok(arr)
-                        }
-                    }
-
-                    let visitor = ArrayVisitor { element: PhantomData };
-                    // The allow is needed to support (32 + 33) like expressions
-                    #[allow(unused_parens)]
-                    deserializer.deserialize_tuple($len, visitor)
-                }
-            }
-        )+
+        pub use $crate::BigArray as $name;
     };
     ($name:ident; + $($len:expr),* $(,)?) => {
         big_array! {
