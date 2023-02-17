020c1aa0590752974d2b9c4c9beb7ef64ac4b85a
est31 <MTest31@outlook.com>
2023-02-17 16:01:17 +0100

    Use assert_eq

--- tests/ser_xor_de.rs
+++ tests/ser_xor_de.rs
@@ -9,7 +9,7 @@ struct S {
     arr: [SerOnly; 64],
 }
 
-#[derive(Clone, Copy, Serialize)]
+#[derive(Debug, Clone, Copy, Serialize)]
 struct SerOnly(u8);
 
 #[derive(Deserialize)]
@@ -18,7 +18,7 @@ struct D {
     arr: [DeOnly; 64],
 }
 
-#[derive(Clone, Copy, Deserialize)]
+#[derive(Debug, Clone, Copy, Deserialize)]
 struct DeOnly(u8);
 
 impl PartialEq<DeOnly> for SerOnly {
@@ -34,5 +34,5 @@ fn test() {
     };
     let j = serde_json::to_string(&s).unwrap();
     let s_back = serde_json::from_str::<D>(&j).unwrap();
-    assert!(&s.arr[..] == &s_back.arr[..]);
+    assert_eq!(&s.arr[..], &s_back.arr[..]);
 }
