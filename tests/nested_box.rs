ebd235e27a193bed6393e5fe258c6df5857dfea2
est31 <MTest31@outlook.com>
2023-03-04 07:37:45 +0100

    Improve the nested test to show how nested arrays work

--- tests/nested_box.rs
+++ tests/nested_box.rs
@@ -1,17 +1,20 @@
 #![no_std]
 
+extern crate alloc;
+
+use alloc::boxed::Box;
 use serde_big_array::Array;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct S {
-    arr: Array<Array<u8, 234>, 65>,
+    arr: Box<Array<u8, 1234>>,
 }
 
 #[test]
 fn test() {
     let s = S {
-        arr: Array([Array([1; 234]); 65]),
+        arr: Box::new(Array([1; 1234])),
     };
     let j = serde_json::to_string(&s).unwrap();
     let s_back = serde_json::from_str::<S>(&j).unwrap();
